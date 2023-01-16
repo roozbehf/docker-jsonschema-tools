ab42c0ad049d939c3b3fec1b7812ba81e0ac826d
Roozbeh Farahbod <dev@roozbeh.ca>
2023-01-16 19:12:20 +0100

    fix: bypass non-yaml-file references

--- files/validate.sh
+++ files/validate.sh
@@ -40,7 +40,7 @@ cd ${ROOT_DIR}
 
 # convert all referenced YAML files 
 DIR_NAME=`dirname ${YAML_SCHEMA}`
-cat ${YAML_SCHEMA}  | grep "\$ref" | sed 's/^[\t\ ]*$ref:\ //g' | sed "s/\'//g" | while read yamlFile; do $0 "${DIR_NAME}/${yamlFile}"; done
+cat ${YAML_SCHEMA}  | grep "\$ref" | grep "ya\?ml" | sed 's/^[\t\ ]*$ref:\ //g' | sed "s/\'//g" | while read yamlFile; do $0 "${DIR_NAME}/${yamlFile}"; done
 
 yaml2json ${YAML_SCHEMA}
 
